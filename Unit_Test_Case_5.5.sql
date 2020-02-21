77d194b1fd897d3c428558bbc1569d65d5262715
ummema.aiman <ummema.aiman@thomsonreuters.com>
2020-02-21 17:29:16 +0530

    Release Notes 5.5.docx Unit_Test_Case_5.5.sql

--- Unit_Test_Case_5.5.sql
+++
@@ -1,8 +0,0 @@
-
-
----to get the orgID from Change DB
-select orgID,CIDGen from Deal_Change..DlOrgChg group
- by orgID,CIDGen having count(CIDGen) >1 order by orgID,CIDGen
- ---To verify the data in table
- select * from Deal..DLOrgChg
- select * from Deal_Update..DlOrgChg
